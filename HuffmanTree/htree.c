2c9d871711cf442bbfaa86e5e67caa292fd82d3c
hjh0915 <623299157@qq.com>
2021-04-28 23:33:01 +0800

    哈夫曼树（未完成）

--- HuffmanTree/htree.c
+++ HuffmanTree/htree.c
@@ -7,53 +7,27 @@ typedef struct ElemType {
     int parent, lchild, rchild;
 } ElemType;
 
-void Select(ElemType huffmanTree[], int min1, int min2, int n);
+void Select(ElemType huffmanTree[], int s1, int s2, int n);
 void PrintHtree(ElemType htree[], int n);
 
 // 找到权值最小的两个根结点
 void Select(ElemType huffmanTree[], int s1, int s2, int n) {
     int min1, min2;
 
-    //遍历数组初始下标为 0
-    int i = 0;
-    //找到还没构建树的结点
-    while(huffmanTree[i].parent != 0 && i <= n) {
-        i++;
+    for (int i=0; i<n; i++) {
+        if (huffmanTree[i].parent == -1) {
+            if (huffmanTree[i].weight < min1) {
+                min2 = min1;
+                s2 = s1;
+                min1 = huffmanTree[i].weight;
+                s1 = i;
+            } 
+            else if (huffmanTree[i].weight < min2) {
+                min2 = huffmanTree[i].weight;
+                s2 = i;
+            }
+        }
     }
-    min1 = huffmanTree[i].weight;
-    s1 = i;
-    
-    while(huffmanTree[i].parent != 0 && i <= n) {
-        i++;
-    }
-
-    //对找到的两个结点比较大小，min2为大的，min1为小的
-    if(huffmanTree[i].weight < min1) {
-        min2 = min1;
-        s2 = s1;
-        min1 = huffmanTree[i].weight;
-        s1 = i;
-    }else {
-        min2 = huffmanTree[i].weight;
-        s2 = i;
-    }
-
-    // //两个结点和后续的所有未构建成树的结点做比较
-    // for(int j=i+1; j<=n; j++) {
-
-    //     //如果比最小的还小，将min2=min1，min1赋值新的结点的下标
-    //     if(huffmanTree[j].weight < min1) {
-    //         min2 = min1;
-    //         min1 = huffmanTree[j].weight;
-    //         s2 = s1;
-    //         s1 = j;
-    //     }
-    //     //如果介于两者之间，min2赋值为新的结点的位置下标
-    //     else if(huffmanTree[j].weight >= min1 && huffmanTree[j].weight < min2) {
-    //         min2 = huffmanTree[j].weight;
-    //         s2 = j;
-    //     }
-    // }
 }
 
 void CreateHtree(ElemType huffmanTree[], int w[], int n) {
@@ -71,11 +45,12 @@ void CreateHtree(ElemType huffmanTree[], int w[], int n) {
 
     for (k=n; k<2*n-1; k++) {
         Select(huffmanTree, i1, i2, k);  //权值最小的两个根结点下标为i1, i2
+
+        huffmanTree[k].weight = huffmanTree[i1].weight + huffmanTree[i2].weight;
         huffmanTree[i1].parent = k;
         huffmanTree[i2].parent = k;
         huffmanTree[k].lchild = i1;
         huffmanTree[k].rchild = i2;
-        huffmanTree[k].weight = huffmanTree[i1].weight + huffmanTree[i2].weight;
     }
 }
 
