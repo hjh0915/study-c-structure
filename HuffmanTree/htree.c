081d40971e286d0d0f658123bf9369f475750860
hjh0915 <623299157@qq.com>
2021-04-28 23:23:42 +0800

    哈夫曼树（未完成）

--- HuffmanTree/htree.c
+++ HuffmanTree/htree.c
@@ -7,7 +7,54 @@ typedef struct ElemType {
     int parent, lchild, rchild;
 } ElemType;
 
-void Select(ElemType huffmanTree[], int min1, int min2);
+void Select(ElemType huffmanTree[], int min1, int min2, int n);
+void PrintHtree(ElemType htree[], int n);
+
+// 找到权值最小的两个根结点
+void Select(ElemType huffmanTree[], int s1, int s2, int n) {
+    int min1, min2;
+
+    //遍历数组初始下标为 0
+    int i = 0;
+    //找到还没构建树的结点
+    while(huffmanTree[i].parent != 0 && i <= n) {
+        i++;
+    }
+    min1 = huffmanTree[i].weight;
+    s1 = i;
+    
+    while(huffmanTree[i].parent != 0 && i <= n) {
+        i++;
+    }
+
+    //对找到的两个结点比较大小，min2为大的，min1为小的
+    if(huffmanTree[i].weight < min1) {
+        min2 = min1;
+        s2 = s1;
+        min1 = huffmanTree[i].weight;
+        s1 = i;
+    }else {
+        min2 = huffmanTree[i].weight;
+        s2 = i;
+    }
+
+    // //两个结点和后续的所有未构建成树的结点做比较
+    // for(int j=i+1; j<=n; j++) {
+
+    //     //如果比最小的还小，将min2=min1，min1赋值新的结点的下标
+    //     if(huffmanTree[j].weight < min1) {
+    //         min2 = min1;
+    //         min1 = huffmanTree[j].weight;
+    //         s2 = s1;
+    //         s1 = j;
+    //     }
+    //     //如果介于两者之间，min2赋值为新的结点的位置下标
+    //     else if(huffmanTree[j].weight >= min1 && huffmanTree[j].weight < min2) {
+    //         min2 = huffmanTree[j].weight;
+    //         s2 = j;
+    //     }
+    // }
+}
 
 void CreateHtree(ElemType huffmanTree[], int w[], int n) {
     int i, k, i1, i2;
@@ -20,74 +67,33 @@ void CreateHtree(ElemType huffmanTree[], int w[], int n) {
 
     for (i=0; i<n; i++) {
         huffmanTree[i].weight = w[i];
-
-        for (k=n; k<2*n-1; k++) {
-            Select(huffmanTree, i1, i2);  //权值最小的两个根结点下标为i1, i2
-            huffmanTree[k].weight = huffmanTree[i1].weight + huffmanTree[i2].weight;
-            huffmanTree[i1].parent =k;
-            huffmanTree[i2].parent = k;
-            huffmanTree[k].lchild = i1;
-            huffmanTree[k].rchild = i2;
-        }
     }
-}
 
-// 找到权值最小的两个根结点
-void Select(ElemType huffmanTree[], int min1, int min2) {
-    int n;
-    int left, right;
-    left = right = 0;
-
-    for (int i=0; i<=n; i++) {
-        //如果是根节点
-        if(huffmanTree[i].parent == 0) {
-            if (huffmanTree[i].weight < min1) {
-                min2 = min1;
-                right = left;
-                min1 = huffmanTree[i].weight;
-                left = i;
-            } else if (huffmanTree[i].weight < min2) {
-                min2 = huffmanTree[i].weight;
-                right = i;
-            }
-        }
-    }
-    //两个结点和后续的所有未构建成树的结点做比较
-    for(int j=i+1; j<=n; j++) {
-        //如果有父结点，直接跳过，进行下一个
-        if(huffmanTree[j].parent != 0) {
-            continue;
-        }
-        //如果比最小的还小，将min2=min1，min1赋值新的结点的下标
-        if(huffmanTree[j].weight < min1) {
-            min2 = min1;
-            min1 = huffmanTree[j].weight;
-            right = left;
-            left = j;
-        }
-        //如果介于两者之间，min2赋值为新的结点的位置下标
-        else if(huffmanTree[j].weight >= min1 && huffmanTree[j].weight < min2) {
-            min2 = huffmanTree[j].weight;
-            right = j;
-        }
+    for (k=n; k<2*n-1; k++) {
+        Select(huffmanTree, i1, i2, k);  //权值最小的两个根结点下标为i1, i2
+        huffmanTree[i1].parent = k;
+        huffmanTree[i2].parent = k;
+        huffmanTree[k].lchild = i1;
+        huffmanTree[k].rchild = i2;
+        huffmanTree[k].weight = huffmanTree[i1].weight + huffmanTree[i2].weight;
     }
 }
 
 //打印输出
 void PrintHtree(ElemType htree[], int n) {
-    for (int i=0; i<=n; i++) {
-        if (htree[i].parent == -1) {
-            
-        }
+
+    for (int i=0; i<n; i++) {
+        printf("%d \n", htree[i].weight);
     }
 }
 
 int main() {
-    int w[5] = {2, 8, 7, 6, 5};
-    int n = 5;
-    ElemType htree;
+    int w[4] = {2, 4, 5, 3};
+    int n = 4;
+    ElemType htree[2*n-1];
 
     CreateHtree(htree, w, n);
+    // PrintHtree(htree, n);
 
     return 0;
 }
